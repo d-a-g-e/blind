e74bb5136b0c2f4fad5dcbcee7f3b8e59b429313
dangaresp <d.garrido.e@gmail.com>
2015-11-12 11:55:49 -0300

    remove rbenv-vars

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ set :repo_url, 'git@github.com:dangaresp/blind.git'
 
 set :deploy_to, '/mnt/app'
 
-set :linked_files, %w{config/database.yml .rbenv-vars}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
